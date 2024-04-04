ae2f56d21718ba911060480dd4930bc360844491
KeremTamirci <htamirci20@ku.edu.tr>
2024-04-04 15:53:40 +0300

    Android firebase config completed

--- immunotheraphy_app/lib/main.dart
+++ immunotheraphy_app/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:immunotheraphy_app/firebase_options.dart';
 import 'package:immunotheraphy_app/screens/choice_screen.dart';
 import 'package:immunotheraphy_app/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
