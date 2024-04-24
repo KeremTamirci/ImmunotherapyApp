8ce8d8270ea21f6f7047d8e464adaf68d0e32c77
HKARATAS20 <HKARATAS20@KU.EDU.TR>
2024-04-25 00:29:48 +0300

    doz grafiği bitmemiş hali

--- immunotheraphy_app/lib/patient/utils/database_controller.dart
+++ immunotheraphy_app/lib/patient/utils/database_controller.dart
@@ -18,4 +18,37 @@ class DatabaseController {
       throw e;
     }
   }
+
+   Future<List<DosageData>> getDosageData() async {
+    try {
+      // Reference to the dosage collection for the current user
+      CollectionReference dosageCollection =
+          FirebaseFirestore.instance.collection('Patients').doc(userId).collection('Dosage Recordings');
+
+      // Retrieve dosage data from Firestore
+      QuerySnapshot snapshot = await dosageCollection.get();
+
+      // Convert Firestore data to DosageData objects
+      List<DosageData> dosageData = snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return DosageData(
+          date: (data['dosage_date'] as Timestamp).toDate(),
+          amount: data['dosage_amount'] as int,
+        );
+      }).toList();
+
+      return dosageData;
+    } catch (e) {
+      print('Failed to retrieve dosage data: $e');
+      // Handle error
+      return [];
+    }
+  }
+}
+
+class DosageData {
+  final DateTime date;
+  final int amount;
+
+  DosageData({required this.date, required this.amount});
 }
