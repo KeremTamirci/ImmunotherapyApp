917163c102bd1985a26b9af0de6ecd7499020d88
ataerden <72795653+ataerden@users.noreply.github.com>
2024-05-14 21:01:43 +0300

    doctor and patient profile page error fixation
    doctor and patient profile pages fixed with bad ui.

--- immunotheraphy_app/lib/patient/screens/profile_page.dart
+++ immunotheraphy_app/lib/patient/screens/profile_page.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:immunotheraphy_app/patient/screens/patient_signin_screen.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -11,9 +12,12 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   late User _user;
+  late Map<String, dynamic> _patientData = {};
+  late Map<String, dynamic> _doctorData = {};
   final Text homeScreenTitle = const Text("Patient Home Screen");
   final Text logOutText = const Text("Log Out");
   final TextStyle style = const TextStyle(fontSize: 20);
+  bool gotData = false;
 
   @override
   void initState() {
@@ -27,34 +31,199 @@ class _ProfilePageState extends State<ProfilePage> {
       setState(() {
         _user = user;
       });
+      await _getPatientData(user.uid);
     }
+  }
+
+  Future<void> _getPatientData(String patientId) async {
+    try {
+      DocumentSnapshot patientSnapshot = await FirebaseFirestore.instance
+          .collection('Patients')
+          .doc(patientId)
+          .get();
+
+      if (patientSnapshot.exists) {
+        setState(() {
+          _patientData = patientSnapshot.data() as Map<String, dynamic>;
+        });
+        await _getDoctorData(_patientData['uid']);
+        gotData = true;
+      }
+    } catch (e) {
+      print('Error fetching patient data: $e');
+    }
+  }
+
+  Future<void> _getDoctorData(String doctorId) async {
+    try {
+      DocumentSnapshot doctorSnapshot = await FirebaseFirestore.instance
+          .collection('Doctors')
+          .doc(doctorId)
+          .get();
+
+      if (doctorSnapshot.exists) {
+        setState(() {
+          _doctorData = doctorSnapshot.data() as Map<String, dynamic>;
+        });
+      }
+    } catch (e) {
+      print('Error fetching doctor data: $e');
+    }
+  }
+
+  String _formatDate(Timestamp timestamp) {
+    DateTime date = timestamp.toDate();
+    return '${date.day} ${_getMonth(date.month)} ${date.year}';
+  }
+
+  String _getMonth(int month) {
+    switch (month) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      case 12:
+        return 'December';
+      default:
+        return '';
+    }
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Are you sure you want to sign out?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Sign Out'),
+              onPressed: () {
+                FirebaseAuth.instance.signOut().then((value) {
+                  print("Signed Out");
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PatientSignInScreen()),
+                  );
+                });
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Welcome, ${_user.displayName ?? 'Guest'}!',
-            style: style,
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            child: logOutText,
-            onPressed: () {
-              FirebaseAuth.instance.signOut().then((value) {
-                print("Signed Out");
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const PatientSignInScreen()),
-                );
-              });
-            },
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: FutureBuilder(
+          future: _getUserData(),
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            if (!gotData) {
+              return const CircularProgressIndicator();
+            } else {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const CircleAvatar(
+                    radius: 60,
+                    backgroundImage: NetworkImage(
+                        'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y&d=mm'),
+                  ),
+                  const SizedBox(height: 20),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: _patientData['first_name'],
+                      style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: ' ' + _patientData['last_name'],
+                          style: const TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Email: ${_user.email}',
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  Text(
+                    'Phone Number: ${_patientData['phone_number']}',
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  Text(
+                    'Has Allergic Rhinitis: ${_patientData['has_allergic_rhinitis'] ? 'Yes' : 'No'}',
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  Text(
+                    'Has Asthma: ${_patientData['has_asthma'] ? 'Yes' : 'No'}',
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  Text(
+                    'Birth Date: ${_formatDate(_patientData['birth_date'])}',
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  Text(
+                    'Doctor: ${_doctorData['first_name']} ${_doctorData['last_name']}',
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    child: logOutText,
+                    onPressed: () {
+                      _confirmSignOut(context);
+                    },
+                  ),
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
