aac8eaca064159c4c645b7e57e60f703369ef902
ataerden <72795653+ataerden@users.noreply.github.com>
2024-05-23 21:45:26 +0300

    end

--- immunotheraphy_app/lib/patient/screens/patient_signin_screen.dart
+++ immunotheraphy_app/lib/patient/screens/patient_signin_screen.dart
@@ -125,10 +125,10 @@ class _PatientSignInScreenState extends State<PatientSignInScreen> {
                 }),
                 signUpOption(),
                 Padding(padding: EdgeInsets.fromLTRB(
-                  20,
-                  MediaQuery.of(context).size.height * 0.18,
-                  20,
-                  20, // Add bottom padding here
+                20,
+                MediaQuery.of(context).size.height * 0.18,
+                20,
+                20, // Add bottom padding here
                 ))
               ],
             ),
