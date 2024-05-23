c71ffc5a9fb23bb227ade515ee26c6409aa58604
ataerden <72795653+ataerden@users.noreply.github.com>
2024-05-23 23:31:12 +0300

    progress

--- immunotheraphy_app/lib/patient/screens/infoSheets/MilkLadderInfoSheet.dart
+++ immunotheraphy_app/lib/patient/screens/infoSheets/MilkLadderInfoSheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,7 +19,12 @@ class MyApp extends StatelessWidget {
                   borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
                 ),
                 builder: (BuildContext context) {
-                  return MilkLadderInfoSheet();
+                  return SlidingUpPanel(
+                    borderRadius: BorderRadius.circular(20.0),
+                    maxHeight: MediaQuery.of(context).size.height * 0.9,
+                    minHeight: 0,
+                    panel: MilkLadderInfoSheet(),
+                  );
                 },
               );
             },
@@ -33,74 +39,74 @@ class MyApp extends StatelessWidget {
 class MilkLadderInfoSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.9,
-        child: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/sut_ana_resim.png"),
-                      fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16.0),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/sut_ana_resim.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Süt Merdiveni Hakkında",
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back_ios_rounded),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Center(
+                child: Text(
+                  "Süt Merdiveni",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "Aşağıdaki diagramda çeşitli süt ürünlerinin içerdiği süt proteini miktarlarını görüp karşılaştırabilirsiniz.",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: RotatedBox(
+                  quarterTurns: 1, // Rotates the image 90 degrees
+                  child: AspectRatio(
+                    aspectRatio: 1.5, // Adjust the aspect ratio as needed
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Image.asset("assets/images/Süt_Merdiveni_İngilizce.png"),
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "Süt Basamağı Hakkında",
-                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.arrow_back_ios_rounded),
-                        onPressed: () => Navigator.of(context).pop(),
-                      ),
-                    ],
-                  ),
                 ),
-                const SizedBox(height: 10),
-                const Center(
-                  child: Text(
-                    "Süt Basamağı",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Milk Semptom Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 20),
-                    ],
-                  ),
-                ),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () => _showImageDialog(context),
-                    child: const Text("Süt Merdiveni Görüntüle"),
-                  ),
-                ),
-                const SizedBox(height: 20),
-              ],
-            ),
+              ),
+              const SizedBox(height: 20),
+            ],
           ),
         ),
       ),
     );
   }
+
+
 
   void _showImageDialog(BuildContext context) {
     showDialog(
